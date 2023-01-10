5e969df9a13c743e2ef75225b20fe35d51e04a6f
Herman <ushkalo.herman@gmail.com>
2023-01-10 21:12:54 +0200

    add file

--- server/index.js
+++ server/index.js
@@ -1,21 +1,26 @@
-const axios = require('axios');
+import Express from 'express';
+import bodyParser from 'body-parser';
+import authRouter from './router/auth-router.js';
+import errorMiddleware from './middlewares/error-middleware.js';
+import * as dotenv from 'dotenv';
+import morgan from 'morgan';
+import cors from 'cors';
+// import path from 'path';
+import cookieParser from 'cookie-parser';
 
-const options = {
-  method: 'POST',
-  url: 'https://qrcode-monkey.p.rapidapi.com/qr/transparent',
-  headers: {
-    'content-type': 'application/json',
-    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-    'X-RapidAPI-Host': 'qrcode-monkey.p.rapidapi.com',
-  },
-  data: '{"data":"https://www.qrcode-monkey.com","x":0,"y":0,"size":400,"crop":true,"download":false,"file":"png"}',
+export default () => {
+  dotenv.config();
+  const app = new Express({ logger: true });
+
+  app.use(cookieParser());
+  app.use(cors());
+
+  app.use(morgan('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  // app.use('/avatars', Express.static(`${path.resolve()}/avatars`));
+  // app.use('/picture-post', Express.static(`${path.resolve()}/picture-post`));
+  app.use('/api/auth', authRouter);
+  app.use(errorMiddleware);
+  return app;
 };
-
-axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error.response.data);
-  });
---
+++ server/migrations/create_comments_table.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+export const up = (knex) =>
+  knex.schema.createTable('comments', (table) => {
+    table.string('id').primary().notNullable();
+    table.string('user_id').unsigned().index().references('users.id');
+    table.string('content').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+  });
+
+export const down = (knex) => knex.schema.dropTable('users');
