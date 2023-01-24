83a275b7669acf95706d305f3fe1019ba1d4141a
Herman <ushkalo.herman@gmail.com>
2023-01-24 23:15:00 +0200

    add event model and controller

--- server/controller/events-controller.js
+++ server/controller/events-controller.js
@@ -1,8 +1,13 @@
 import ApiError from '../exceptions/api-error.js';
 import { v4 as uuidv4 } from 'uuid';
+import Event from '../models/Event.js';
 
-class Events{
-  
+class Events {
+  getAllEvents() {}
+  getEventsById(id) {}
+  createEvent(data) {}
+  updateEvent(data) {}
+  deleteEvent(data) {}
 }
 
 export default new Events();
---
+++ server/models/Event.js
@@ -0,0 +1,89 @@
+import client from '../client.js';
+
+class Event {
+  async findId(id) {
+    const data = await client('events').select('*').where('id', '=', id);
+    if (data.length === 0) {
+      throw ApiError.NotFound('event not found');
+    }
+    return data[0];
+  }
+
+  async getAll() {
+    const data = await client('events').select('*');
+    return data;
+  }
+
+  async save({
+    id,
+    userId,
+    title,
+    description,
+    category,
+    city,
+    address,
+    poster,
+    eventStart,
+    eventEnd,
+  }) {
+    try {
+      await client('events').insert({
+        id,
+        user_id: userId,
+        title,
+        description,
+        category,
+        city,
+        address,
+        poster,
+        event_start: eventStart,
+        event_end: eventEnd,
+      });
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async isEqualTitle(title) {
+    try {
+      const data = await client('events')
+        .select('*')
+        .where('title', '=', title);
+      return data.length !== 0;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async update(id, date) {
+    try {
+      await client('events').where('id', '=', id).update({
+        login: date.login,
+        // password: date.password,
+        full_name: date.fullName,
+        email: date.email,
+        profile_pic: date.avatar,
+      });
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async drop(id) {
+    try {
+      await client('users').where('id', '=', id).del();
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async logout(id) {
+    try {
+      await client('users').where('id', '=', id).update('token', null);
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+
+export default new Event();
