4cb56daaafeb48b95788c90f7745b7118c21ad48
Herman <ushkalo.herman@gmail.com>
2023-01-25 10:41:12 +0200

    Update adminRoutes.js

--- client/src/routes/client/adminRoutes.js
+++ client/src/routes/client/adminRoutes.js
@@ -6,24 +6,30 @@ export default {
   fullMainPagePath: () => [clientPath, 'admin'].join('/'),
   mainPagePath: () => '/admin/',
 
+  
   // Users
   fullUsersPagePath: () => [clientPath, 'admin', 'users'].join('/'),
   usersPagePath: () => '/admin/users',
 
+  
   // Roles
   fullRolesPagePath: () => [clientPath, 'admin', 'roles'].join('/'),
   rolesPagePath: () => '/admin/roles',
+
 
   // Events
   fullEventsPagePath: () => [clientPath, 'admin', 'events'].join('/'),
   eventsPagePath: () => '/admin/events',
 
-  fullEventsEditPagePath: () => [clientPath, 'admin', 'events'].join('/'),
+  fullEventsEditPagePath: (eventId) => [clientPath, 'admin', 'events', eventId].join('/'),
+  eventsEditPagePath: (eventId) => '/admin/events',
+
 
   // Categories
   fullCategoriesPagePath: () => [clientPath, 'admin', 'categories'].join('/'),
   categoriesPagePath: () => '/admin/categories',
 
+  
   // Tickets
   fullTicketsPagePath: () => [clientPath, 'admin', 'tickets'].join('/'),
   ticketsPagePath: () => '/admin/tickets'
