889cd350a826a06638f7f863fbfd4ce9a08885b2
Herman <ushkalo.herman@gmail.com>
2023-01-27 01:22:17 +0200

    add file validation create event

---
+++ server/middlewares/validationError.event.js
@@ -0,0 +1,10 @@
+import ApiError from '../exceptions/api-error.js';
+import { validationResult } from 'express-validator';
+
+export default async (req, _res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(ApiError.BadRequest('Validation error', errors.array()));
+  }
+  next();
+};
