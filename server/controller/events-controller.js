17b4cb1f3cccf18b27331d789cbe9463926c9349
Herman <ushkalo.herman@gmail.com>
2023-01-27 01:21:53 +0200

    change controlle function

--- server/controller/events-controller.js
+++ server/controller/events-controller.js
@@ -1,23 +1,23 @@
-import { v4 as uuidv4 } from 'uuid';
-
-import Event from '../models/Event.js';
-import ApiError from '../exceptions/api-error.js';
+import EventService from '../services/event.service.js';
 
 class Events {
+  constructor() {
+    this.eventService = new EventService();
+  }
+
   async getAllEvents(req, res) {
     const events = await Event.getAllEvents();
     res.status(200);
     res.json({ events });
   }
-  async getEventsById(id) {}
-  async createEvent(req, res) {
-    const id = uuidv4();
-    const userId = uuidv4();
-    await Event.save({ id, userId, ...req.body });
-    res.json({
-      massage: 'create event',
-    });
+
+  async getEventsById(req, res) {}
+
+  async createEvent(req, _res) {
+    const result = await this.eventService.createEvent(req.body);
+    return result;
   }
+
   async updateEvent(data) {}
   async deleteEvent(data) {}
 }
