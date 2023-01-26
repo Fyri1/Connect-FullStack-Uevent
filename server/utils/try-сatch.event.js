ad5a7e87344061aa0988b8a495a1309a16056380
Herman <ushkalo.herman@gmail.com>
2023-01-27 01:19:13 +0200

    add try catch event

--- server/utils/try-сatch.event.js
+++ server/utils/try-сatch.event.js
@@ -4,12 +4,11 @@ const tryCatch = (controller) => async (req, res, next) => {
   try {
     const result = await controller(req, res);
     res.status(200).json({ values: result || 'Ok' });
-  }
-  catch (error) {
+  } catch (error) {
     if (error.code === 11000)
       return res.json(ApiError.BadRequest('Validation error', errors.array()));
+    return next(error);
   }
-  return next(error);
-}
+};
 
 export default tryCatch;
