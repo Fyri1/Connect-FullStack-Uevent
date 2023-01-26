a57eeabb00d6ab820ccb546ddf98f4d885115095
Herman <ushkalo.herman@gmail.com>
2023-01-27 01:22:34 +0200

    Update event-router.js

--- server/router/event-router.js
+++ server/router/event-router.js
@@ -3,13 +3,12 @@ import { body } from 'express-validator';
 
 import Events from '../controller/events-controller.js';
 import adminRoutes from '../routes/admin-routes.js';
+import validationErrorEvent from '../middlewares/validationError.event.js';
+import tryCatch from '../utils/try-сatch.event.js';
 
 const router = Express.Router();
 
-router.get(
-  adminRoutes.eventsGetPath(),
-  Events.getAllEvents
-);
+router.get(adminRoutes.eventsGetPath(), Events.getAllEvents);
 router.post(
   adminRoutes.eventPostPath(),
   body('title').isEmpty().isLength({ min: 3, max: 30 }).trim(),
@@ -19,6 +18,8 @@ router.post(
   body('address').isEmpty().isLength({ min: 10, max: 30 }).trim(),
   body('eventStart').isEmpty().trim(),
   body('eventEnd').isEmpty().trim(),
+  validationErrorEvent,
+  tryCatch(Events.createEvent),
   Events.createEvent
 );
 
