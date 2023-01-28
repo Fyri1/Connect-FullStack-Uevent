38e9fd4c8fb898de3f35e0339832717b1477a324
Herman <ushkalo.herman@gmail.com>
2023-01-28 21:08:30 +0200

    fix and new table tickets

--- server/migrations/create_ticket_table.js
+++ server/migrations/create_ticket_table.js
@@ -1,12 +1,11 @@
 // @ts-check
 
 export const up = (knex) =>
-  knex.schema.createTable('tikets', (table) => {
+  knex.schema.createTable('ticket', (table) => {
     table.string('id').primary().notNullable();
     table.string('event_id').unsigned().index().references('event.id');
     table.decimal('price').notNullable();
-    table.integer('sell_ticket').notNullable();
-    table.integer('count_ticket').notNullable();
+    table.boolean('is_sold').notNullable().defaultTo(false);
   });
 
 export const down = (knex) => knex.schema.dropTable('users');
