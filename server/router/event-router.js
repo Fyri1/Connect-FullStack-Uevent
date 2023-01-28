9f0213d2551f6e588108ee95cbc3870fd35f76ef
Herman <ushkalo.herman@gmail.com>
2023-01-28 21:11:27 +0200

    Update event-router.js

--- server/router/event-router.js
+++ server/router/event-router.js
@@ -13,18 +13,37 @@ router.get(
   tryCatch(Events.getAllEvents),
   Events.getAllEvents
 );
+
+router.get(
+  adminRoutes.eventTicketsGetPath(),
+  tryCatch(Events.getAllEventTickets),
+  Events.getAllEventTickets
+);
+
 router.post(
   adminRoutes.eventPostPath(),
   body('title').notEmpty().isLength({ min: 3, max: 30 }).trim(),
   body('description').notEmpty().isLength({ min: 10, max: 150 }).trim(),
   body('category').notEmpty(),
-  body('city').notEmpty().isLength({ min: 5, max: 30 }).trim(),
+  body('city').notEmpty().isLength({ min: 2, max: 30 }).trim(),
   body('address').notEmpty().isLength({ min: 3, max: 30 }).trim(),
   body('eventStart').notEmpty().trim(),
   body('eventEnd').notEmpty().trim(),
   validationErrorEvent,
   tryCatch(Events.createEvent),
   Events.createEvent
+);
+
+router.post(
+  adminRoutes.eventSellTicketEvent(),
+  tryCatch(Events.sellTicketEvent),
+  Events.sellTicketEvent
+);
+
+router.post(
+  adminRoutes.eventSellTicketEvent(),
+  tryCatch(Events.sellTicketEvent),
+  Events.sellTicketEvent
 );
 
 export default router;
