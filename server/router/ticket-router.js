110ad6f88d2b18cf0792d26d574cc2e14945a507
Herman <ushkalo.herman@gmail.com>
2023-01-28 21:11:22 +0200

    Create ticket-router.js

---
+++ server/router/ticket-router.js
@@ -0,0 +1,20 @@
+import Express from 'express';
+import { body } from 'express-validator';
+
+import ticketsController from '../controller/tickets-controller.js';
+import adminRoutes from '../routes/admin-routes.js';
+import validationErrorTicket from '../middlewares/validationError.ticket.js';
+import tryCatch from '../utils/try-сatch.event.js';
+
+const router = Express.Router();
+
+router.post(
+  adminRoutes.ticketPostPath(),
+  body('count').notEmpty(),
+  body('price').notEmpty().trim(),
+  validationErrorTicket,
+  tryCatch(ticketsController.createTickets),
+  ticketsController.createTickets
+);
+
+export default router;
