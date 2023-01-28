db0b640d5ee6d778e15aa21b1aaf3681fa4f7094
Herman <ushkalo.herman@gmail.com>
2023-01-28 21:07:51 +0200

    change event validation

--- server/middlewares/validationError.event.js
+++ server/middlewares/validationError.event.js
@@ -1,8 +1,18 @@
 import ApiError from '../exceptions/api-error.js';
 import { validationResult } from 'express-validator';
+import Event from '../models/Event.js';
 
 export default async (req, _res, next) => {
   const errors = validationResult(req);
+
+  if (await Event.isEqualTitle(req.body.title)) {
+    errors.errors.push({
+      value: req.body.title,
+      msg: 'Already exist',
+      param: 'title',
+      location: 'body',
+    });
+  }
 
   if (!errors.isEmpty()) {
     return next(ApiError.BadRequest('Validation error', errors.array()));
