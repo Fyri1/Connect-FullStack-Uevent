4e88f367f92b0f38cfa336d3c792c5d4284a939a
Herman <ushkalo.herman@gmail.com>
2023-01-28 21:07:25 +0200

    add ticket router

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import cookieParser from 'cookie-parser';
 
 import authRouter from './router/auth-router.js';
 import eventRouter from './router/event-router.js';
+import ticketsRouter from './router/ticket-router.js';
 import adminRouter from './router/admin-router.js';
 import errorMiddleware from './middlewares/error-middleware.js';
 
@@ -30,6 +31,7 @@ export default () => {
   // app.use('/picture-post', Express.static(`${path.resolve()}/picture-post`));
   app.use('/api/auth', authRouter);
   app.use('/api/event', eventRouter);
+  app.use('/api/ticket', ticketsRouter);
   app.use('/api/user', adminRouter);
   app.use(errorMiddleware);
   return app;
