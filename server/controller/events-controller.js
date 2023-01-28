0e87c3a8e515f3008e063be61fcea5bf6320fe15
Herman <ushkalo.herman@gmail.com>
2023-01-28 21:05:26 +0200

    add token service and valid token

--- server/controller/events-controller.js
+++ server/controller/events-controller.js
@@ -1,4 +1,5 @@
 import EventService from '../services/event.service.js';
+import TokenService from '../services/token-service.js';
 
 class Events {
   async getAllEvents(_req, _res) {
@@ -6,10 +7,38 @@ class Events {
     return result;
   }
 
+  async getAllEventTickets(req, _res) {
+    const { id: eventId } = req.params;
+    const result = await EventService.getAllEventTickets(eventId);
+    return {
+      tickets: result,
+      count: result.length,
+      ticketsLeft: result.filter((i) => !i.is_sold).length,
+    };
+  }
+
+  async sellTicketEvent(req, _res) {
+    const { id: eventId } = req.params;
+    const token = req.headers.authorization.split(' ')[1];
+    const { id: userId } = TokenService.validateAccessToken(token);
+    const result = await EventService.sellTicket(userId, eventId);
+    return result;
+  }
+
+  async returnTicketEvent(req, _res) {
+    const { id: ticketId } = req.params;
+    const token = req.headers.authorization.split(' ')[1];
+    TokenService.validateAccessToken(token);
+    const result = await EventService.ticketReturn(ticketId);
+    return result;
+  }
+
   async getEventsById(req, res) {}
 
   async createEvent(req, _res) {
-    const result = await EventService.createEvent(req.body);
+    const token = req.headers.authorization.split(' ')[1];
+    const { id } = TokenService.validateAccessToken(token);
+    const result = await EventService.createEvent({ ...req.body, userId: id });
     return result;
   }
 
