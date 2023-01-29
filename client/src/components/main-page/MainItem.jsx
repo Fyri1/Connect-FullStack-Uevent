2037962bc31ef26c0c70f209158781a135a35ecd
VILSAN <49275632+Fyri1@users.noreply.github.com>
2023-01-29 03:00:11 +0200

    add

--- client/src/components/main-page/MainItem.jsx
+++ client/src/components/main-page/MainItem.jsx
@@ -6,8 +6,8 @@ const MainItem = () => {
     <div сlass="py-6 px-4 sm:p-6 md:py-10 md:px-8">
       <div class="max-w-4xl mx-auto grid grid-cols-1 lg:max-w-5xl lg:gap-x-20 lg:grid-cols-2">
         <div class="relative p-3 col-start-1 row-start-1 flex flex-col-reverse rounded-lg bg-gradient-to-t from-black/75 via-black/0 sm:bg-none sm:row-start-2 sm:p-0 lg:row-start-1">
-          <h1 class="mt-1 text-lg font-semibold text-white sm:text-slate-900 md:text-2xl dark:sm:text-white">Дом на пляже в Коллингвуде</h1>
-          <p class="text-sm leading-4 font-medium text-white sm:text-slate-500 dark:sm:text-slate-400">Весь дом</p>
+          <h1 class="mt-1 text-lg font-semibold text-white sm:text-slate-900 md:text-2xl dark:sm:text-white">Ticket to Poland</h1>
+          {/* <p class="text-sm leading-4 font-medium text-white sm:text-slate-500 dark:sm:text-slate-400">Весь дом</p> */}
         </div>
         <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
           <img src="/beach-house.jpg" alt="" class="w-full h-60 object-cover rounded-lg sm:h-52 sm:col-span-2 lg:col-span-full" loading="lazy"></img>
@@ -22,26 +22,70 @@ const MainItem = () => {
             </svg>
             <span>4.89 <span class="text-slate-400 font-normal">(128)</span></span>
           </dd>
+          
           <dt class="sr-only">Место расположения</dt>
           <dd class="flex items-center">
             <svg width="2" height="2" aria-hidden="true" fill="currentColor" class="mx-3 text-slate-300">
               <circle cx="1" cy="1" r="1" />
             </svg>
+            {/* иконка локации */}
             <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-1 text-slate-400 dark:text-slate-500" aria-hidden="true">
               <path d="M18 11.034C18 14.897 12 19 12 19s-6-4.103-6-7.966C6 7.655 8.819 5 12 5s6 2.655 6 6.034Z" />
               <path d="M14 11a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z" />
             </svg>
-            Коллингвуд, Онтарио
+              <a class="text-lg font-semibold text-white sm:text-slate-900 md:text-1xl dark:sm:text-white" >Łwow</a>
           </dd>
+          
         </dl>
+        
+        {/* то что идет после описания */}
+        {/* начало  */}
+        <div class="mt-5 text-xs font-medium flex items-center row-start-2 sm:mt-1 sm:row-start-3 md:mt-2.5 lg:row-start-5">
+          <dd class="flex items-center">
+            <svg width="2" height="2" aria-hidden="true" fill="currentColor" class="mx-0 text-slate-300">
+              <circle cx="1" cy="1" r="1" />
+            </svg>
+           
+            <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="mr-1 text-slate-400 dark:text-slate-500" aria-hidden="true">
+              <path d="M18 11.034C18 14.897 12 19 12 19s-6-4.103-6-7.966C6 7.655 8.819 5 12 5s6 2.655 6 6.034Z" />
+              <path d="M14 11a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z" />
+            </svg>
+              <a class="text-lg font-semibold text-white sm:text-slate-900 md:text-1xl dark:sm:text-white" >Start :<a>  14:88</a></a>
+              
+          </dd>
+          <dd class="flex items-center">
+            <svg width="2" height="2" aria-hidden="true" fill="currentColor" class="mx-0 text-slate-300">
+              <circle cx="1" cy="1" r="1" />
+            </svg>
+           
+            <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="mr-1 text-slate-400 dark:text-slate-500" aria-hidden="true">
+              <path d="M18 11.034C18 14.897 12 19 12 19s-6-4.103-6-7.966C6 7.655 8.819 5 12 5s6 2.655 6 6.034Z" />
+              <path d="M14 11a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z" />
+            </svg>
+              <a class="text-lg font-semibold text-white sm:text-slate-900 md:text-1xl dark:sm:text-white" >END :<a>  14:88</a></a>
+          </dd>
+        </div>
+
+        
+         
         <div class="mt-4 col-start-1 row-start-3 self-center sm:mt-0 sm:col-start-2 sm:row-start-2 sm:row-span-2 lg:mt-6 lg:col-start-1 lg:row-start-3 lg:row-end-4">
-          <button type="button" class="bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg">Buy now</button>
+         
+          <h4 class="mt-1 text-lg font-semibold text-white sm:text-slate-800 md:text-2xl dark:sm:text-white">Prise:<a>  56$</a></h4>
+
+          <button type="button" class="bg-indigo-600 text-white text-sm leading-7 font-medium py-2 px-4 rounded-lg">Buy now</button>
         </div>
         
         <p class="mt-4 text-sm leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400">
           Этот солнечный и просторный номер предназначен для тех, кто путешествует налегке и ищет удобное и уютное место, чтобы прилечь на ночь или две. Этот пляжный дом расположен в оживленном районе со множеством кафе, пабов, ресторанов и супермаркетов, недалеко от всех основных достопримечательностей, таких как Эдинбургский замок и Артурс-Сит.
-        </p>
+        </p>  
+        {/* <a href="#" className="fa fa-map-marker">_
+           we Łwowie
+        </a> */}
       </div>
+      <div>
+
+      </div>
+
     </div>
   );
 }
