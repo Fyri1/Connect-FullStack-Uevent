3e112229c09f370516901c023f375072bfc9810a
VILSAN <49275632+Fyri1@users.noreply.github.com>
2023-02-14 00:00:27 +0200

    Card start

--- client/src/components/payment/PayForm.jsx
+++ client/src/components/payment/PayForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './PayForm.css';
 
 
 
@@ -8,105 +9,130 @@ const MyTicket = () => {
   
   return (
     <div >
-          <div class="py-16 px-4 md:px-6 2xl:px-0 flex justify-center items-center 2xl:mx-auto 2xl:container">
-{/* <!--- more free and premium Tailwind CSS components at https://tailwinduikit.com/ ---> */}
-            <div class="flex flex-col justify-start items-start w-full space-y-9">
-                <div class="flex justify-start flex-col items-start space-y-2">
-                    <button class="flex flex-row items-center text-gray-600 dark:text-white hover:text-gray-500 space-x-1">
-                        <svg class="fill-stroke" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M2.91681 7H11.0835" stroke="currentColor" stroke-width="0.666667" stroke-linecap="round" stroke-linejoin="round" />
-                            <path d="M2.91681 7L5.25014 9.33333" stroke="currentColor" stroke-width="0.666667" stroke-linecap="round" stroke-linejoin="round" />
-                            <path d="M2.91681 7.00002L5.25014 4.66669" stroke="currentColor" stroke-width="0.666667" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                        <p class="text-sm leading-none">Back</p>
+        <div class="bg-blue-50">
+                <div class="m-4">
+                <div class="credit-card w-full sm:w-auto shadow-lg mx-auto rounded-xl bg-white" x-data="creditCard">
+                    <header class="flex flex-col justify-center items-center">
+                    <div
+                        class="relative"
+                        // x-show="card === 'front'"
+                        // x-transition:enter="transition ease-out duration-300"
+                        // x-transition:enter-start="opacity-0 transform scale-90"
+                        // x-transition:enter-end="opacity-100 transform scale-100"
+                    >
+                        <img class="w-full h-auto" src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/svg-cards/card-visa-front.png" alt="front credit card"></img>
+                        <div class="front bg-transparent text-lg w-full text-white px-12 absolute left-0 bottom-12">
+                        <p class="number mb-5 sm:text-xl" x-text="cardNumber !== '' ? cardNumber : '0000 0000 0000 0000'"></p>
+                        <div class="flex flex-row justify-between">
+                            <p x-text="cardholder !== '' ? cardholder : 'Card holder'"></p>
+                            <div class="">
+                            {/* <span x-text="expired.month"></span>
+                            <span x-show="expired.month !== ''">/</span>
+                            <span x-text="expired.year"></span> */}
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+
+                    <ul class="flex">
+                        <li class="mx-2">
+                        <img class="w-16" src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/computop.png" alt="" />
+                        </li>
+                        <li class="mx-2">
+                        <img class="w-14" src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/verified-by-visa.png" alt="" />
+                        </li>
+                        <li class="ml-5">
+                        <img class="w-7" src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/mastercard-id-check.png" alt="" />
+                        </li>
+                    </ul>
+                    </header>
+                    <main class="mt-4 p-4">
+                    <h1 class="text-xl font-semibold text-gray-700 text-center">Card payment</h1>
+                    <div class="">
+                        <div class="my-3">
+                        <input
+                            type="text"
+                            class="block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
+                            placeholder="Card holder"
+                            maxlength="22"
+                            x-model="cardholder"
+                        />
+                        </div>
+                        <div class="my-3">
+                        <input
+                            type="text"
+                            class="block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
+                            placeholder="Card number"
+                            // x-model="cardNumber"
+                            // x-on:keydown="format()"
+                            // x-on:keyup="isValid()"
+                            // maxlength="19"
+                        />
+                        </div>
+                        <div class="my-3 flex flex-col">
+                        <div class="mb-2">
+                            <label for="" class="text-gray-700">Expired</label>
+                        </div>
+                        <div class="grid grid-cols-2 sm:grid-cols-4 gap-2">
+                            <select
+                            name=""
+                            id=""
+                            class="form-select appearance-none block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
+                            x-model="expired.month"
+                            >
+                            <option value="" selected disabled>MM</option>
+                            <option value="01">01</option>
+                            <option value="02">02</option>
+                            <option value="03">03</option>
+                            <option value="04">04</option>
+                            <option value="05">05</option>
+                            <option value="06">06</option>
+                            <option value="07">07</option>
+                            <option value="08">08</option>
+                            <option value="09">09</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                            </select>
+                            <select
+                            name=""
+                            id=""
+                            class="form-select appearance-none block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
+                            x-model="expired.year"
+                            >
+                            <option value="" selected disabled>YY</option>
+                            <option value="2021">2021</option>
+                            <option value="2022">2022</option>
+                            <option value="2023">2023</option>
+                            <option value="2024">2024</option>
+                            <option value="2025">2025</option>
+                            <option value="2026">2026</option>
+                            </select>
+                            <input
+                            type="text"
+                            class="block w-full col-span-2 px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
+                            placeholder="Security code"
+                            maxlength="3"
+                            // x-model="securityCode"
+                            // x-on:focus="card = 'back'"
+                            // x-on:blur="card = 'front'"
+                            />
+                        </div>
+                        </div>
+                    </div>
+                    </main>
+                    <footer class="mt-6 p-4">
+                    <button
+                        class="submit-button px-4 py-3 rounded-full bg-blue-300 text-blue-900 focus:ring focus:outline-none w-full text-xl font-semibold transition-colors"
+                        // x-bind:disabled="!isValid"
+                        // x-on:click="onSubmit()"
+                    >
+                        Pay now
                     </button>
-                    <p class="text-3xl lg:text-4xl font-semibold leading-7 lg:leading-9 text-gray-800 dark:text-gray-50">Checkout</p>
-                    <p class="text-base leading-normal sm:leading-4 text-gray-600 dark:text-white">Home  Electronics  Headphones  Cart Checkout</p>
+                    </footer>
                 </div>
-    
-                <div class="flex flex-col xl:flex-row justify-center xl:justify-between space-y-6 xl:space-y-0 xl:space-x-6 w-full">
-                    <div class="xl:w-3/5 flex flex-col sm:flex-row xl:flex-col justify-center items-center bg-gray-100 dark:bg-gray-800 py-7 sm:py-0 xl:py-10 px-10 xl:w-full">
-                        <div class="flex flex-col justify-start items-start w-full space-y-4">
-                            <p class="text-xl md:text-2xl leading-normal text-gray-800 dark:text-gray-50">Logitech K251</p>
-                            <p class="text-base font-semibold leading-none text-gray-600 dark:text-white">$520.00</p>
-                        </div>
-                        <div class="mt-6 sm:mt-0 xl:my-10 xl:px-20 w-52 sm:w-96 xl:w-auto">
-                            <img src="https://i.ibb.co/0GFzTP4/Rectangle-131.png" alt="headphones" />
-                        </div>
-                    </div>
-    
-                    <div class="p-8 bg-gray-100 dark:bg-gray-800 flex flex-col lg:w-full xl:w-3/5">
-                        <button class="border border-transparent hover:border-gray-300 bg-gray-900 dark:bg-white dark:hover:bg-gray-900 dark:hover:border-gray-900 dark:text-gray-900 dark:hover:text-white hover:bg-white text-white hover:text-gray-900 flex flex-row justify-center items-center space-x-2 py-4 rounded w-full">
-                            <div>
-                                <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M10.9099 4.27692C9.6499 4.27692 9.1174 4.87817 8.2399 4.87817C7.34021 4.87817 6.65396 4.28129 5.56208 4.28129C4.49333 4.28129 3.35365 4.93379 2.6299 6.04535C1.61365 7.61285 1.78615 10.565 3.43208 13.08C4.02083 13.9804 4.80708 14.99 5.83833 15.001H5.85708C6.75333 15.001 7.01958 14.4141 8.25302 14.4072H8.27177C9.48677 14.4072 9.73052 14.9975 10.623 14.9975H10.6418C11.673 14.9866 12.5015 13.8679 13.0902 12.971C13.514 12.326 13.6715 12.0022 13.9965 11.2725C11.6155 10.3688 11.233 6.99348 13.5877 5.69942C12.869 4.79942 11.859 4.27817 10.9068 4.27817L10.9099 4.27692Z"
-                                        fill="currentColor"
-                                    />
-                                    <path d="M10.6338 1C9.88379 1.05094 9.00879 1.52844 8.49629 2.15188C8.03129 2.71688 7.64879 3.555 7.79879 4.36781H7.85879C8.65754 4.36781 9.47504 3.88688 9.95254 3.27063C10.4125 2.68406 10.7613 1.85281 10.6338 1V1Z" fill="currentColor" />
-                                </svg>
-                            </div>
-                            <div>
-                                <p class="text-base leading-4">Pay</p>
-                            </div>
-                        </button>
-    
-                        <div class="flex flex-row justify-center items-center mt-6">
-                            <hr class="border w-full" />
-                            <p class="flex flex-shrink-0 px-4 text-base leading-4 text-gray-600 dark:text-white">or pay with card</p>
-                            <hr class="border w-full" />
-                        </div>
-    
-                        <div class="mt-8">
-                            <input class="border border-gray-300 p-4 rounded w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="email" name="" id="" placeholder="Email" />
-                        </div>
-    
-                        <label class="mt-8 text-base leading-4 text-gray-800 dark:text-gray-50">Card details</label>
-                        <div class="mt-2 flex-col">
-                            <div>
-                                <input class="border rounded-tl rounded-tr border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="email" name="" id="" placeholder="0000 1234 6549 15151" />
-                            </div>
-                            <div class="flex-row flex">
-                                <input class="border rounded-bl border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="email" name="" id="" placeholder="MM/YY" />
-                                <input class="border rounded-br border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="email" name="" id="" placeholder="CVC" />
-                            </div>
-                        </div>
-    
-                        <label class="mt-8 text-base leading-4 text-gray-800 dark:text-gray-50">Name on card</label>
-                        <div class="mt-2 flex-col">
-                            <div>
-                                <input class="border rounded border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="email" name="" id="" placeholder="Name on card" />
-                            </div>
-                        </div>
-    
-                        <label class="mt-8 text-base leading-4 text-gray-800 dark:text-gray-50">Country or region</label>
-                        <div class="mt-2 flex-col">
-                            <div class="relative">
-                                <button id="changetext" class="text-left border rounded-tr rounded-tl border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600 bg-white" type="email" name="" id="">United States</button>
-                                <svg onclick="showMenu(true)" id="closeIcon" class="cursor-pointer absolute top-4 right-4" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M3.5 5.75L8 10.25L12.5 5.75" stroke="#27272A" stroke-linecap="round" stroke-linejoin="round" />
-                                </svg>
-                                <svg onclick="showMenu(true)" id="openIcon" class="cursor-pointer hidden transform rotate-180 absolute top-4 right-4" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M3.5 5.75L8 10.25L12.5 5.75" stroke="#27272A" stroke-linecap="round" stroke-linejoin="round" />
-                                </svg>
-                                <div id="dropdown" class="mt-1 hidden absolute z-10 w-full flex bg-gray-50 justify-start flex-col text-gray-600">
-                                    <div onclick="changeText('China')" class="cursor-pointer hover:bg-gray-800 hover:text-white px-4 py-2">China</div>
-                                    <div onclick="changeText('Russia')" class="cursor-pointer hover:bg-gray-800 hover:text-white px-4 py-2">Russia</div>
-                                    <div onclick="changeText('UK')" class="cursor-pointer hover:bg-gray-800 hover:text-white px-4 py-2">UK</div>
-                                </div>
-                            </div>
-                            <input class="border rounded-bl rounded-br border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="text" name="" id="" placeholder="ZIP" />
-                        </div>
-    
-                        <button class="mt-8 border border-transparent hover:border-gray-300 dark:bg-white dark:hover:bg-gray-900 dark:text-gray-900 dark:hover:text-white dark:border-transparent bg-gray-900 hover:bg-white text-white hover:text-gray-900 flex justify-center items-center py-4 rounded w-full">
-                            <div>
-                                <p class="text-base leading-4">Pay </p>
-                            </div>
-                        </button>
-                    </div>
                 </div>
-            </div>
-        </div>
+    </div>
   
     </div>
   );
