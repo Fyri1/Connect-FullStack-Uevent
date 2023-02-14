1aae814694b4d5952e21afe86c275887a0d25e52
VILSAN <49275632+Fyri1@users.noreply.github.com>
2023-02-15 01:58:58 +0200

    card

--- client/src/components/payment/PayForm.jsx
+++ client/src/components/payment/PayForm.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import './PayForm.css';
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css"/>
 
+{/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css"/> */}
 
 
 const MyTicket = () => {
@@ -8,125 +10,77 @@ const MyTicket = () => {
 
   
   return (
-    <div >
-        <div class="bg-blue-50">
-                <div class="m-4">
-                <div class="credit-card w-full sm:w-auto shadow-lg mx-auto rounded-xl bg-white" x-data="creditCard">
-                    <header class="flex flex-col justify-center items-center">
-                    <div
-                        class="relative"
-                        // x-show="card === 'front'"
-                        // x-transition:enter="transition ease-out duration-300"
-                        // x-transition:enter-start="opacity-0 transform scale-90"
-                        // x-transition:enter-end="opacity-100 transform scale-100"
-                    >
-                        <img class="w-full h-auto" src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/svg-cards/card-visa-front.png" alt="front credit card"></img>
-                        <div class="front bg-transparent text-lg w-full text-white px-12 absolute left-0 bottom-12">
-                        <p class="number mb-5 sm:text-xl" x-text="cardNumber !== '' ? cardNumber : '0000 0000 0000 0000'"></p>
-                        <div class="flex flex-row justify-between">
-                            <p x-text="cardholder !== '' ? cardholder : 'Card holder'"></p>
-                            <div class="">
-                            {/* <span x-text="expired.month"></span>
-                            <span x-show="expired.month !== ''">/</span>
-                            <span x-text="expired.year"></span> */}
-                            </div>
-                        </div>
-                        </div>
-                    </div>
+	<div>
+			<div className="min-w-screen min-h-screen bg-gray-200 flex items-center justify-center px-5 pb-10 pt-16">
+						<div className="first-div w-full mx-auto rounded-lg bg-white shadow-lg p-5 text-gray-700">
+							<div className="w-full pt-1 pb-5">
+									<div className="bg-indigo-500 text-white overflow-hidden rounded-full w-20 h-20 -mt-16 mx-auto shadow-lg flex justify-center items-center">
+											<i className="mdi mdi-credit-card-outline text-3xl"></i>
+									</div>
+							</div>
+							<div className="mb-10">
+									<h1 className="text-center font-bold text-xl uppercase">Secure payment info</h1>
+							</div>
+							<div className="mb-10">
+									<h1 className="">Описание:</h1>
+							</div>
+							<div className="mb-10">
+									<h1 className="">Всего:</h1>
+							</div>
+							<div className="mb-3 flex -mx-2">
+									{/* <div className="px-2">
+											<label for="type1" className="flex items-center cursor-pointer">
+													<input type="radio" className="form-radio h-5 w-5 text-indigo-500" name="type" id="type1" checked></input>
+													<img src="https://leadershipmemphis.org/wp-content/uploads/2020/08/780370.png" className="h-8 ml-3"></img>
+											</label>
+									</div> */}
+									<div className="px-2">
+											<label for="type2" className="flex items-center cursor-pointer">
+													<input type="radio" className="form-radio h-5 w-5 text-indigo-500" name="type" id="type2"></input>
+													<img src="https://leadershipmemphis.org/wp-content/uploads/2020/08/780370.png" className="h-8 ml-3"></img>
+											</label>
+									</div>
+							</div>
+							{/* <div className="mb-3">
+									<label className="font-bold text-sm mb-2 ml-1">Name on card</label>
+									<div>
+											<input className="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="John Smith" type="text"/>
+									</div>
+							</div> */}
+							<div className="card-div">
+							<div class="mb-3">
+            <label class="font-bold text-sm mb-2 ml-1">Card number:</label>
+            <div>
+                <input class="w-50 px-3 py-2 mb-1 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="0000 0000 0000 0000" type="text"/>
+								<input class="w-50 " placeholder="MM/YY" type="text"/>
 
-                    <ul class="flex">
-                        <li class="mx-2">
-                        <img class="w-16" src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/computop.png" alt="" />
-                        </li>
-                        <li class="mx-2">
-                        <img class="w-14" src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/verified-by-visa.png" alt="" />
-                        </li>
-                        <li class="ml-5">
-                        <img class="w-7" src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/mastercard-id-check.png" alt="" />
-                        </li>
-                    </ul>
-                    </header>
-                    <main class="mt-4 p-4">
-                    <h1 class="text-xl font-semibold text-gray-700 text-center">Card payment</h1>
-                    <div class="">
+					  </div>
+						
+        </div>
 
-                        <div class="my-3">
-                        <input
-                            type="text"
-                            class="block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
-                            placeholder="Card number"
-                            // x-model="cardNumber"
-                            // x-on:keydown="format()"
-                            // x-on:keyup="isValid()"
-                            // maxlength="19"
-                        />
-                        </div>
-                        <div class="my-3 flex flex-col">
-                        <div class="mb-2">
-                            <label for="" class="text-gray-700">Expired</label>
-                        </div>
-                        <div class="grid grid-cols-2 sm:grid-cols-4 gap-2">
-                            <select
-                            name=""
-                            id=""
-                            class="form-select appearance-none block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
-                            x-model="expired.month"
-                            >
-                            <option value="" selected disabled>MM</option>
-                            <option value="01">01</option>
-                            <option value="02">02</option>
-                            <option value="03">03</option>
-                            <option value="04">04</option>
-                            <option value="05">05</option>
-                            <option value="06">06</option>
-                            <option value="07">07</option>
-                            <option value="08">08</option>
-                            <option value="09">09</option>
-                            <option value="10">10</option>
-                            <option value="11">11</option>
-                            <option value="12">12</option>
-                            </select>
-                            <select
-                            name=""
-                            id=""
-                            class="form-select appearance-none block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
-                            x-model="expired.year"
-                            >
-                            <option value="" selected disabled>YY</option>
-                            <option value="2021">2021</option>
-                            <option value="2022">2022</option>
-                            <option value="2023">2023</option>
-                            <option value="2024">2024</option>
-                            <option value="2025">2025</option>
-                            <option value="2026">2026</option>
-                            </select>
-                            <input
-                            type="text"
-                            class="block w-full col-span-2 px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
-                            placeholder="Security code"
-                            maxlength="3"
-                            // x-model="securityCode"
-                            // x-on:focus="card = 'back'"
-                            // x-on:blur="card = 'front'"
-                            />
-                        </div>
-                        </div>
-                    </div>
-                    </main>
-                    <footer class="mt-6 p-4">
-                    <button
-                        class="submit-button px-4 py-3 rounded-full bg-blue-300 text-blue-900 focus:ring focus:outline-none w-full text-xl font-semibold transition-colors"
-                        // x-bind:disabled="!isValid"
-                        // x-on:click="onSubmit()"
-                    >
-                        Pay now
-                    </button>
-                    </footer>
-                </div>
-                </div>
-    </div>
-  
-    </div>
+								<div classname="mb-10">
+										<label classname="font-bold text-sm mb-2 ml-1">Security code</label>
+										<div>
+												<input className="w-32 px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="000" type="text"/>
+										</div>
+								</div>
+					</div>
+							<div>
+									<button className="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold"><i className="mdi mdi-lock-outline mr-1"></i> PAY NOW</button>
+							</div>
+					</div>
+		</div>
+
+			{/* <!-- BUY ME A BEER AND HELP SUPPORT OPEN-SOURCE RESOURCES --> */}
+			<div className="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
+					<div>
+							<a title="Buy me a beer" href="https://www.buymeacoffee.com/scottwindon" target="_blank" className="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12">
+									<img className="object-cover object-center w-full h-full rounded-full" src="https://i.pinimg.com/originals/60/fd/e8/60fde811b6be57094e0abc69d9c2622a.jpg"/>
+							</a>
+					</div>
+			</div>
+
+	</div>
   );
 }
 
