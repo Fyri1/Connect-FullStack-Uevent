14bd24cdba84e548453652e8c555e7a16169824b
VILSAN <49275632+Fyri1@users.noreply.github.com>
2023-02-16 00:22:19 +0200

    Update PayForm.jsx

--- client/src/components/payment/PayForm.jsx
+++ client/src/components/payment/PayForm.jsx
@@ -82,7 +82,7 @@ const MyTicket = () => {
       <div class="footer footer-center  w-full p-4 bg-gray-300 text-gray-800">
         <div class="text-center">
           <p>
-            Copyright © 2022 -
+            Copyright © 2022.02.24 -
             <a class="font-semibold" href="mailto:m.sohanemon@gmail.com"
               >SohanEmon</a>
           </p>
