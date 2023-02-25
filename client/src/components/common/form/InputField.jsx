5ccec5d9195e29befc38e89a8c175eab81a391e0
Tymofeii <tima130603@gmail.com>
2023-02-25 18:34:22 -0300

    Fixed input error popover bug

--- client/src/components/common/form/InputField.jsx
+++ client/src/components/common/form/InputField.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ErrorPopover from './ErrorPopover.jsx';
+import ToastWarning from '../toast/ToastWarning.jsx';
 
 
 const InputField = ({ id, name, type, data, setData, errors, setErrors, children }) => {
@@ -54,7 +54,9 @@ const InputField = ({ id, name, type, data, setData, errors, setErrors, children
         <input onChange={inputChangeHandle} type={type} name="floating_email" id={id} className="block py-2.5 px-7 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " value={data[id]}/>
         <label htmlFor="floating_email" className="px-10 peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-95 peer-focus:-translate-y-6 peer-focus:-translate-x-8">{name}</label>
       </div>
-      <ErrorPopover id={"error-" + id} errorTitle={"Oops! " + errors[id]} errorDescription={errors[id]} />
+      <div data-popover id={"error-" + id} role="tooltip" className="absolute z-10 invisible inline-block w-64 text-sm font-light text-gray-500 transition-opacity duration-300 opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
+        <ToastWarning id={"toast-warning-" + id} message={"Oops! " + errors[id]} errorDescription={errors[id]} />
+      </div>
     </div>
   );
 };
