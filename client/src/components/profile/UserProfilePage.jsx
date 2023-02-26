56017c3c3ba5f14c6421cb186600e0d50100c1c4
Tymofeii <tima130603@gmail.com>
2023-02-26 01:55:56 -0300

    Test slider animation

--- client/src/components/profile/UserProfilePage.jsx
+++ client/src/components/profile/UserProfilePage.jsx
@@ -9,7 +9,6 @@ import ToastWarning from '../common/toast/ToastWarning.jsx';
 import ToastError from '../common/toast/ToastError.jsx';
 
 import '../css/Profile.css';
-import data from '../../temp/user_data.json';
 import { useUserProfile } from '../../../hooks/useUserProfile.js';
 
 
@@ -39,12 +38,89 @@ const UserProfilePage = () => {
           <div className="h-full w-[60%] ml-[1%]"><UserEventsTabsSelector userData={userData} setUserData={setUserData} isLoading={isLoading} /></div>
         </div>
         
-        <div className='fixed bottom-0 right-4 w-72'>
+        {/* <div className='fixed bottom-0 right-4 w-72'>
           <ToastSuccess id="gavno1" message="sasi dayn nice" />
           <ToastWarning id="gavno2" message="sasi dayn gavno" />
           <ToastError id="gavno3" message="sasi dayn error" />
-        </div>
+        </div> */}
         
+        <div className="my-4 flex flex-col"> 
+          <div className="w-full flex flex-col">
+            <div className="flex-1 bg-white rounded-lg shadow-xl py-3 px-6" data-carousel="static" id="indicators-carousel">
+              
+              <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
+                <ul className="flex flex-wrap -mb-px text-sm font-medium text-center">
+                  <li className="mr-2" role="presentation">
+                    <button aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0" className="inline-block p-4 border-b-2 rounded-t-lg" id={"Profile" + "-tab"} type="button" >Profile</button>
+                  </li>
+
+                  <li className="mr-2" role="presentation">
+                    <button aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1" className="inline-block p-4 border-b-2 rounded-t-lg" id={"Email" + "-tab"} type="button" >Email</button>
+                  </li>
+
+                  <li className="mr-2" role="presentation">
+                    <button aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2" className="inline-block p-4 border-b-2 rounded-t-lg" id={"Pass" + "-tab"} type="button" >Pass</button>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="hidden duration-700 ease-in-out" data-carousel-item="active">
+                <img src="/docs/images/carousel/carousel-1.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."></img>
+                <p>Parasha1</p>
+              </div>
+              
+              <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                <img src="/docs/images/carousel/carousel-2.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."></img>
+                <p>Parasha2</p>
+              </div>
+              
+              <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                <img src="/docs/images/carousel/carousel-3.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."></img>
+                <p>Parasha3</p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+
+        {/* <div id="indicators-carousel" className="relative" data-carousel="static">
+          <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+            <div className="hidden duration-700 ease-in-out" data-carousel-item="active">
+              <img src="/docs/images/carousel/carousel-1.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."></img>
+              <p>Parasha1</p>
+            </div>
+            
+            <div className="hidden duration-700 ease-in-out" data-carousel-item>
+              <img src="/docs/images/carousel/carousel-2.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."></img>
+              <p>Parasha2</p>
+            </div>
+            
+            <div className="hidden duration-700 ease-in-out" data-carousel-item>
+              <img src="/docs/images/carousel/carousel-3.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."></img>
+              <p>Parasha3</p>
+            </div>
+            
+            <div className="hidden duration-700 ease-in-out" data-carousel-item>
+              <img src="/docs/images/carousel/carousel-4.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."></img>
+              <p>Parasha4</p>
+            </div>
+            
+            <div className="hidden duration-700 ease-in-out" data-carousel-item>
+              <img src="/docs/images/carousel/carousel-5.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."></img>
+              <p>Parasha5</p>
+            </div>
+          </div>
+          
+          <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+            <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+            <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+            <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+            <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
+            <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
+          </div>
+        </div> */}
+
       </div>
     </div>
   );
