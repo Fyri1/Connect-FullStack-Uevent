c4ef24a02d573cbc38fb2eb532bd8d23ab204789
Tymofeii <tima130603@gmail.com>
2023-03-13 23:19:59 -0400

    Comment edit button handle update

--- client/src/components/event-page/comments/Comment.jsx
+++ client/src/components/event-page/comments/Comment.jsx
@@ -31,7 +31,7 @@ const Comment = ({ data }) => {
 
 
   return isLoading ? <></> : (
-    <div className="items-center ml-[22%] mr-[22%] rounded-2xl p-8 shadow-lg hover:shadow-2xl transition duration-500">
+    <div className="items-center ml-[20%] mr-[20%] rounded-2xl p-8 shadow-lg hover:shadow-2xl transition duration-500">
       <div className="flex items-center space-x-2">
         {/* User ava */}
         <div className="flex flex-shrink-0 self-start cursor-pointer">
@@ -40,21 +40,47 @@ const Comment = ({ data }) => {
 
         <div className="flex items-center justify-center w-full space-x-3">
           <div className="block w-full">
-            <div className="bg-gray-100 w-full rounded px-2 pb-2 pt-1">
+            <div className="bg-gray-100 w-full rounded px-2 pb-2">
               <div className="flex text-sm font-semibold">
-                <span className="font-bold">{!user['first_name'] ? user.login : `${user['first_name']} ${user['second_name']}`}</span>
-                <span className="font-normal pl-1"> • {datePublish}</span>
-
-                <button className={`flex p-1.5 mr-2 transition-all duration-300 rounded-3xl text-gray-400 hover:bg-gray-200`}>
-                  <svg className="h-2.5 w-2.5" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"/>
-                  </svg>
-                </button>
+                <span className="font-bold pt-2">{!user['first_name'] ? user.login : `${user['first_name']} ${user['second_name']}`}</span>
+                <span className="font-normal pl-1 pt-2"> • {datePublish}</span>
+                <div className="pt-1.5">
+                {
+                  show ? (
+                    <div className="flex">
+                      <button onClick={() => setShow(!show)} className={`flex p-1.5 ml-2 transition-all duration-300 rounded-3xl text-gray-600 hover:bg-gray-200`}>
+                        <svg className="h-3 w-3" fill="none" viewBox="0 0 23 23" stroke="currentColor" strokeWidth="2" xmlns="http://www.w3.org/2000/svg">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                        </svg>
+                      </button>
+                      <button onClick={() => { setShow(!show), setContent(data.content) }} className={`flex p-1.5 mr-2 transition-all duration-300 rounded-3xl text-gray-400 hover:bg-gray-200`}>
+                        <svg className="h-3 w-3" fill="none" viewBox="0 0 23 23" stroke="currentColor" strokeWidth="2" xmlns="http://www.w3.org/2000/svg" >
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                      </button>
+                    </div>
+                  ) : (
+                    <button onClick={() => setShow(!show)} className={`flex p-1.5 ml-2 transition-all duration-300 rounded-3xl text-gray-400 hover:bg-gray-200`}>
+                      <svg className="h-3 w-3" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"/>
+                      </svg>
+                    </button>
+                  )
+                }
+                </div>
               </div>
 
-              <div className="">
-                <p className="text-sm text-gray-600 md:text-base ">{data.content}</p>
-              </div>
+              <form onSubmit={handleForm}>
+                <div>
+                  {
+                    show ? (
+                      <textarea onChange={(e) => setContent(e.target.value)} value={content} className="h-40 px-3 text-sm py-1 mt-5 outline-none border-gray-300 w-full resize-none border rounded-lg placeholder:text-sm" placeholder="Add your comments here" />
+                    ) : ( 
+                      <p className="text-md text-gray-600">{data.content}</p>
+                    )
+                  }
+                </div>
+              </form>
 
               <div className="flex">
                 <ReactionElement reactionClickHandle={likeButtonClickHandle} isActive={true} reactionAmount={23}>
@@ -70,30 +96,7 @@ const Comment = ({ data }) => {
         </div>
       </div>
 
-      <form onSubmit={handleForm}>
-        <div className="mt-4">
-          {
-            show ? (
-              <textarea onChange={(e) => setContent(e.target.value)} value={content} className="h-40 px-3 text-sm py-1 mt-5 outline-none border-gray-300 w-full resize-none border rounded-lg placeholder:text-sm" placeholder="Add your comments here" />
-            ) : ( 
-              <p className="mt-4 text-md text-gray-600">{data.content}</p>
-            )
-          }
-        </div>
-
-        <div className="flex justify-between items-end">
-          {
-            show ? (
-              <div className="flex">
-                <button onClick={() => setShow(!show)} className="px-3 py-1 bg-blue-600 text-sm text-white rounded-lg transition-all cursor-pointer hover:bg-blue-500">Change</button>
-                <button onClick={() => { setShow(!show), setContent(data.content) }} className="px-3 py-1 bg-blue-600 text-sm text-white rounded-lg transition-all cursor-pointer hover:bg-blue-500">Cancel</button>
-              </div>
-            ) : (
-              <button onClick={() => setShow(!show)} className="px-3 py-1 bg-blue-600 text-sm text-white rounded-lg transition-all cursor-pointer hover:bg-blue-500">Edit</button>
-            )
-          }
-        </div>
-      </form>
+      
     </div>
   );
 };
