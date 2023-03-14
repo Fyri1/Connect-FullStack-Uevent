c80491ded1c665c5306779bdc2fd0c2d30513a10
VILSAN <49275632+Fyri1@users.noreply.github.com>
2023-03-14 23:07:13 +0200

    Update Success.jsx

--- client/src/components/common/Success.jsx
+++ client/src/components/common/Success.jsx
@@ -32,19 +32,29 @@ const Success = () => {
             {/* <!-- Description --> */}
             <p class="text-center text-gray-700 font-light mt-5"> Your order has been successfully completed go nagyi! </p>
 
-            
+            <div class='flex items-center justify-center  '>
+                <div class="p-10">
+                  <button type="button" class="flex items-center rounded-lg bg-blue-500 px-4 py-2 text-white" disabled>
+                    <svg class="-ml-1 mr-3 h-5 w-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                    <span class="font-medium subpixel-antialiased">Processing...</span>
+                  </button>
+                </div>
+
+            </div>
 
             {/* <!-- Buttons --> */}
-            <div class="flex flex-row-reverse mt-14 items-end gap-5">
+            <div class=" flex justify-center flex-row-reverse mt-14 items-end gap-5">
 
-              <button class="hover:bg-blue-700 bg-blue-600 text-gray-100 p-4 rounded">
-                Good
-              </button>
+            <button type="button" class="text-blue-700 font-normal py-2 px-4 
+            rounded transition duration-300 ease-in-out focus:outline-none 
+            focus:shadow-outline border border-blue-700 hover:bg-blue-700 hover:text-white">Выйти НАХУЙ</button>
 
-                {/* добавлб стрелку  */}
-              <button class="text-gray-600 hover:bg-gray-100 hover:text-gray-700 py-4 px-2">
-               Сome back
-              </button>
+
+    
+
 
             </div>
 
