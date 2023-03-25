71d654ccf250095bff0f6d795e17fa26638bc91e
VILSAN <49275632+Fyri1@users.noreply.github.com>
2023-03-25 16:53:04 +0200

    )))))))))))))))))))

--- client/src/components/main-page/EventElement.jsx
+++ client/src/components/main-page/EventElement.jsx
@@ -6,15 +6,15 @@ import '../css/event-card.css';
 const EventElement = ({ event }) => {
   return (
     <div >
-      <div className=" px-10 py-10 gap-8 snap-x  cursor-pointer  ">
-        {/* сделаю в ряд много картинок */}
+      <div className="px-3 py-5 gap-8 snap-x  cursor-pointer  ">
+       
         <div className="">
           <div className="col-sm-4  flex-col rounded-[20px]
-            
-            w-full !p-4 3xl:p-![18px] ">
+            hover:bg-wh-700  bg-gray-0 p-2 rounded-lg group relative
+            w-full !p-3 3xl:p-![18px] ">
               <div className="h-full w-full">
-                {/* тут добавлю размер картинки  */}
-                  <div className="relative  w-full h-full "> 
+                
+                  <div className=" w-full h-full "> 
                       {/* <img src="https://horizon-tailwind-react-git-tailwind-components-horizon-ui.vercel.app/static/media/Nft3.3b3e6a4b3ada7618de6c.png" class="mb-3 h-full w-full rounded-xl 3xl:h-full 3xl:w-full" alt=""/> */}
                       <img src="https://i1.sndcdn.com/avatars-000630927555-iux64b-t500x500.jpg" alt="" />
                       
@@ -44,6 +44,7 @@ const EventElement = ({ event }) => {
                           <a className="text-lg font-semibold  sm:text-slate-900 md:text-1xl " >&nbsp; 14 квитня 18:00, ПТ</a>
                               
                         </div>
+                        {/* name event */}
                         <p className='name text-gray-700 font-bold text-2xl mb-3 hover:text-gray-900 hover:cursor-pointer'>{event.title}</p>
 
                         {/* <p class="text-lg font-bold text-navy-700"> Abstract Colors </p> */}
@@ -64,7 +65,7 @@ const EventElement = ({ event }) => {
                       
 
                   </div>
-                  <div className="justify-between md:items-center lg:justify-between ">
+                  {/* <div className="justify-between md:items-center lg:justify-between ">
 
                     <div className='items-center'>
                       <a href={`/event/${event.id}`} className="">
@@ -76,13 +77,53 @@ const EventElement = ({ event }) => {
                       </a>
 
                       </div>     
+                  </div>          */}
+              </div>
+              {/* slider on ticket */}
+              <div class=" absolute bg-black rounded bg-opacity-0 group-hover:bg-opacity-60 
+                w-full h-full right-0 top-0   
+                group-hover:opacity-100 duration-700 transition flex flex-col justify-between">
+                <div  class=" mt-5 hover:scale-10 text-white outline-none opacity-0
+                  group-hover:opacity-100 ">
+                    {/* I will think something BLYAT */}
+                  <button class="button-rekomend-main p-4 rounded-full transition ease-in duration-200 focus:outline-none">
+                  <svg viewBox="0 0 20 20" enable-background="new 0 0 20 20" class="w-6 h-6">
+                      <path fill="#FFFFFF" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                  </svg>
+                  </button>
+                </div>
+                <div  class="  hover:scale-10 text-white outline-none opacity-0
+                  group-hover:opacity-100 ">
+                  <a className="text-lg   md:text-1xl " > 14 квитня</a>
+                </div>
+
+                <div  class="hover:scale-10 text-white outline-none opacity-0
+                  group-hover:opacity-100 ">
+                  <p className='name text-wight-700 font-bold text-2xl mb-3 hover:text-wight-900 hover:cursor-pointer'>{event.title}</p>
+                </div>
+
+                <div class="mb-10 hover:scale-10 text-white outline-none opacity-0
+                  group-hover:opacity-100  ">
+                  <p className=' text-wight-700    text-lg  text-white '>{event.city}</p>
+                  <div className=" justify-between md:items-center lg:justify-between ">
+                    <div className='items-center'>
+                      <a href={`/event/${event.id}`} className="">
+                        <button className=" fa fa-shopping-cart p-2 pl-11 pr-11 
+                        bg-transparent border-2 border-indigo-500 text-indigo-500 text-lg rounded-lg 
+                        transition-colors duration-700 transform hover:bg-indigo-500 hover:text-gray-100 
+                        focus:border-4 focus:border-indigo-300">КУПЫТЫ</button>
+                      </a>
+                      </div>     
                   </div>         
+                </div>
+                
               </div>
           </div>
+
         </div>
 
-      </div>
 
+      </div>
 
     </div>
   );
