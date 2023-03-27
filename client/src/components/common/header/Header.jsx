8e77bd0a8beac56429e619af6c082a7b4a6f7642
VILSAN <49275632+Fyri1@users.noreply.github.com>
2023-03-27 21:13:26 +0300

    1

--- client/src/components/common/header/Header.jsx
+++ client/src/components/common/header/Header.jsx
@@ -6,6 +6,9 @@ import AuthButton from './AuthButton.jsx';
 import UserDropdownMenu from './UserDropdownMenu.jsx';
 import LanguageSelectMenu from './LanguageSelectMenu.jsx';
 import clientRoutes from '../../../routes/client/clientRoutes.js';
+// C:\Users\maxdz\Desktop\Connect-FullStack-Uevent\client\src\components\main-page\SearchInput.jsx
+import SearchInput from "../../main-page/SearchInput.jsx";
+
 import '../../css/header.css';
 
 
@@ -21,6 +24,7 @@ const Header = () => {
   return (
     <header className="h-10">
       <nav className="fixed z-50 w-full bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-dark-bg-800">
+        
         <div className=" flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
 
           <a href={clientRoutes.mainPagePath()} className="flex items-center">
@@ -39,6 +43,8 @@ const Header = () => {
           <Navbar />
           
           <div className=" lg:order-1">
+          
+           
             {
               // check whether user is logged in
               localStorage.getItem('token')
@@ -67,6 +73,8 @@ const Header = () => {
               <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
               <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
             </button>
+            {/* <SearchInput placeholder="Enter event name" setSearchInput={setSearchInput} /> */}
+
           </div>
         </div>
       </nav>
