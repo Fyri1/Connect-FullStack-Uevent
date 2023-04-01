b4050bb28cca3123699b97a11f8a6d199df96fec
VILSAN <49275632+Fyri1@users.noreply.github.com>
2023-04-02 01:22:19 +0300

    1

--- client/src/components/profile/coupon/Coupon.jsx
+++ client/src/components/profile/coupon/Coupon.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Active from './Active.jsx';
+import { useTranslation } from 'react-i18next';
 const Coupon = () => {
   return (
     <div>
@@ -36,6 +38,7 @@ const Coupon = () => {
             placeholder="Search for elixir..."
             class="pl-16 pr-4 py-4 rounded-md shadow-md bg-white border-0 w-full outline-none"
           />
+
           <button class="w-10 h-10 border rounded-lg p-1 cursor-pointer transition duration-200 text-blue-800 hover:bg-blue-100">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -50,87 +53,23 @@ const Coupon = () => {
                 d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z"
               />
             </svg>
+            
           </button>
+
         </div>
-
         {/* Active Promo */}
-        <ul class="   left-0 right-0 -bottom-18 mt-3 p-3">
-          <li class="text-xs uppercase text-gray-400 border-b border-gray border-solid py-2 px-5 mb-2">
-            Active
-          </li>
-          <div className=" shadow-lg  hover:shadow-xl transform hover: transition duration-500 bg-gray-100 mx-auto border-gray-500 border rounded-sm  text-gray-700 mb-0.5">
-            <div className="flex p-3  border-l-8 border-green-600">
-              <div className="space-y-1 border-r-2 pr-3">
-                <div className="text-sm leading-5 font-semibold">
-                  <span className="text-xs leading-4 font-normal text-gray-500">
-                    {' '}
-                    Release #
-                  </span>{' '}
-                  LTC08762304
-                </div>
-                <div className="text-sm leading-5 font-semibold">
-                  <span className="text-xs leading-4 font-normal text-gray-500 pr">
-                    {' '}
-                    BOL #
-                  </span>{' '}
-                  10937
-                </div>
-                <div className="text-sm leading-5 font-semibold">
-                  JUN 14. 9:30 PM
-                </div>
-              </div>
-              <div className="flex-1">
-                <div className="ml-3 space-y-1 border-r-2 pr-3">
-                  <div className="text-base leading-6 font-normal">
-                    KROGER MEMPHIS
-                  </div>
-                  <div className="text-sm leading-4 font-normal">
-                    <span className="text-xs leading-4 font-normal text-gray-500">
-                      {' '}
-                      Carrier
-                    </span>{' '}
-                    PAPER TRANSPORT INC.
-                  </div>
-                  <div className="text-sm leading-4 font-normal">
-                    <span className="text-xs leading-4 font-normal text-gray-500">
-                      {' '}
-                      Destination
-                    </span>{' '}
-                    WestRock Jacksonville - 9469 Eastport Rd, Jacksonville, FL
-                    32218
-                  </div>
-                </div>
-              </div>
-              <div className="border-r-2 pr-3">
-                <div>
-                  <div className="ml-3 my-3 border-gray-200 border-2 bg-gray-300 p-1">
-                    <div className="uppercase text-xs leading-4 font-medium">
-                      Trailer
-                    </div>
-                    <div className="text-center text-sm leading-4 font-semibold text-gray-800">
-                      89732
-                    </div>
-                  </div>
-                </div>
-              </div>
+          <Active>
+          </Active>
+        
+    
 
-              <div>
-                <button className=" rounded-sm  ml-2 ">
-                  <div className="ml-3 my-5 bg-green-600 p-1 w-20">
-                    <div className="uppercase text-xs leading-4 font-semibold text-center text-green-100">
-                      Picked UP
-                    </div>
-                  </div>
-                </button>
-              </div>
-            </div>
-          </div>
-        </ul>
         {/* no active */}
         <ul class="   left-0 right-0 -bottom-18 mt-3 p-3">
+
           <li class="text-xs uppercase text-gray-400 border-b border-gray border-solid py-2 px-5 mb-2">
             NO Active
           </li>
+          
           <div class="bg-gray-100 mx-auto border-gray-500 border rounded-sm  text-gray-700 mb-0.5">
             <div class="flex p-3  border-l-8 border-red-600">
               <div class="space-y-1 border-r-2 pr-3">
@@ -148,41 +87,23 @@ const Coupon = () => {
                   </span>{' '}
                   10937
                 </div>
-                <div class="text-sm leading-5 font-semibold">
-                  JUN 14. 9:30 PM
-                </div>
+
               </div>
               <div class="flex-1">
                 <div class="ml-3 space-y-1 border-r-2 pr-3">
-                  <div class="text-base leading-6 font-normal">
-                    KROGER MEMPHIS
-                  </div>
-                  <div class="text-sm leading-4 font-normal">
-                    <span class="text-xs leading-4 font-normal text-gray-500">
-                      {' '}
-                      Carrier
-                    </span>{' '}
-                    PAPER TRANSPORT INC.
-                  </div>
-                  <div class="text-sm leading-4 font-normal">
-                    <span class="text-xs leading-4 font-normal text-gray-500">
-                      {' '}
-                      Destination
-                    </span>{' '}
-                    WestRock Jacksonville - 9469 Eastport Rd, Jacksonville, FL
-                    32218
-                  </div>
+                <div class="mt-6 text-sm leading-5 font-semibold">
+                  JUN 14. 9:30 PM
+                </div>
                 </div>
               </div>
               <div class="border-r-2 pr-3">
                 <div>
-                  <div class="ml-3 my-3 border-gray-200 border-2 bg-gray-300 p-1">
-                    <div class="uppercase text-xs leading-4 font-medium">
-                      Trailer
+                  <div className="ml-4 my-4 border-gray-200 border-2 bg-gray-300 p-1">
+
+                    <div className="text-center text-2xl leading-5 font-semibold text-gray-800">
+                      20%
                     </div>
-                    <div class="text-center text-sm leading-4 font-semibold text-gray-800">
-                      89732
-                    </div>
+
                   </div>
                 </div>
               </div>
---
+++ client/src/components/profile/coupon/CreateCupon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+const CreateCupon = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default CreateCupon;
