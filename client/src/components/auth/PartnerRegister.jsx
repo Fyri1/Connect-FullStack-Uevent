07f22777c483027b6b65be313db2e49739d58161
Tymofeii <tima130603@gmail.com>
2023-04-01 22:28:06 -0400

    Code cleanup

--- client/src/components/auth/PartnerRegister.jsx
+++ client/src/components/auth/PartnerRegister.jsx
@@ -112,9 +112,6 @@ const PartnerRegister = () => {
             </div>
             
           </InputField>
-          <InputField className="ml-3 text-sm">
-                <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
-              </InputField>
 
           <button onClick={() => { console.log(partnerData) }} type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
             Submit
