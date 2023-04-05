3e7661da03e95ba14b4de2a8e8512e45e707cffc
Herman <ushkalo.herman@gmail.com>
2023-04-06 02:54:14 +0300

    change use term

--- client/src/components/auth/partner-register/partner-components/page-componenrs/TermUse.jsx
+++ client/src/components/auth/partner-register/partner-components/page-componenrs/TermUse.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
+import { useState } from 'react';
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from '@material-tailwind/react';
+
+const info1 = [
+  {
+    head: 'как пососал?',
+    body: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus minus, veritatis, voluptatem ex fugit unde rem quasi doloribus consectetur ducimus quis molestias esse cum nemo eveniet maxime, debitis voluptatum nam!`,
+  },
+  {
+    head: 'как поесть говна?',
+    body: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus minus, veritatis, voluptatem ex fugit unde rem quasi doloribus consectetur ducimus quis molestias esse cum nemo eveniet maxime, debitis voluptatum nam!`,
+  },
+  {
+    head: 'как обосрать админа?',
+    body: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus minus, veritatis, voluptatem ex fugit unde rem quasi doloribus consectetur ducimus quis molestias esse cum nemo eveniet maxime, debitis voluptatum nam!`,
+  },
+];
+const info2 = [
+  {
+    head: 'как правльно копировать формы?',
+    body: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus minus, veritatis, voluptatem ex fugit unde rem quasi doloribus consectetur ducimus quis molestias esse cum nemo eveniet maxime, debitis voluptatum nam!`,
+  },
+  {
+    head: 'как не писать кнопку в ссылке?',
+    body: `Берешь и не пишешь :D`,
+  },
+  {
+    head: 'как захватить Украину за 3 дня?',
+    body: `Спросить у Макса.`,
+  },
+]
+
+const AccordionComponent = ({ head, body }) => {
+  const [open, setOpen] = useState(false);
+
+  const svg = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={`${open ? 'rotate-180' : ''} h-5 w-5 transition-transform`}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+    </svg>
+  );
+
+  return (
+    <Accordion open={open} icon={svg} className='bg-gray-100'>
+      <AccordionHeader onClick={() => setOpen(!open)} className='border-none'>{head}</AccordionHeader>
+      <AccordionBody>{body}</AccordionBody>
+    </Accordion>
+  );
+};
 
 const TermUse = () => {
-  return  (
-    <div className="animate-active-page">
-      <div>
-        <section className="text-gray-700">
-          <div className="container px-5 py-24 mx-auto">
+  return (
+    <div className="animate-active-page min-h-full">
+        <section className="text-gray-700 h-1/2">
+          <div className="container px-5 py-16 mx-auto">
             <div className="text-center mb-20">
               <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">
                 Frequently Asked Question
@@ -17,80 +74,24 @@ const TermUse = () => {
             </div>
             <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
               <div className="w-full lg:w-1/2 px-4 py-2">
-                <details className="mb-4">
-                  <summary className="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                    How Long is this site live?
-                  </summary>
-
-                  <span>
-                    Laboris qui labore cillum culpa in sunt quis sint veniam.
-                    Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                    minim velit nostrud pariatur culpa magna in aute.
-                  </span>
-                </details>
-                <details className="mb-4">
-                  <summary className="font-semibold bg-gray-200 rounded-md py-2 px-4">
-                    Can I install/upload anything I want on there?
-                  </summary>
-
-                  <span>
-                    Laboris qui labore cillum culpa in sunt quis sint veniam.
-                    Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                    minim velit nostrud pariatur culpa magna in aute.
-                  </span>
-                </details>
-                <details className="mb-4">
-                  <summary className="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                    How can I migrate to another site?
-                  </summary>
-
-                  <span>
-                    Laboris qui labore cillum culpa in sunt quis sint veniam.
-                    Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                    minim velit nostrud pariatur culpa magna in aute.
-                  </span>
-                </details>
+                {info1.map(({ head, body }) => (
+                    <div className="mb-4">
+                    <AccordionComponent head={head} body={body} />
+                    </div>
+                ))}
               </div>
               <div className="w-full lg:w-1/2 px-4 py-2">
-                <details className="mb-4">
-                  <summary className="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                    Can I change the domain you give me?
-                  </summary>
-
-                  <span className="px-4 py-2">
-                    Laboris qui labore cillum culpa in sunt quis sint veniam.
-                    Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                    minim velit nostrud pariatur culpa magna in aute.
-                  </span>
-                </details>
-                <details className="mb-4">
-                  <summary className="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                    How many sites I can create at once?
-                  </summary>
-
-                  <span className="px-4 py-2">
-                    Laboris qui labore cillum culpa in sunt quis sint veniam.
-                    Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                    minim velit nostrud pariatur culpa magna in aute.
-                  </span>
-                </details>
-                <details className="mb-4">
-                  <summary className="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                    How can I communicate with you?
-                  </summary>
-
-                  <span className="px-4 py-2">
-                    Laboris qui labore cillum culpa in sunt quis sint veniam.
-                    Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                    minim velit nostrud pariatur culpa magna in aute.
-                  </span>
-                </details>
+                {info2.map(({ head, body }) => (
+                    <div className="mb-4">
+                    <AccordionComponent head={head} body={body} />
+                    </div>
+                ))}
               </div>
             </div>
           </div>
         </section>
-      </div>
-    </div>);
-  };
+    </div>
+  );
+};
 
 export default TermUse;
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,68 +1,121 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/flowbite/**/*.js',
   ],
   darkMode: 'class',
   theme: {
     extend: {
       keyframes: {
         inactive: {
-          '0%': { opacity: '1',  },
-          '100%': { opacity: '0'},
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
         },
         active: {
           '0%': { opacity: '0' },
-          '100%': { opacity: '1'},
+          '100%': { opacity: '1' },
         },
-        "stape-line-active": {
-          '0%': { width: '0%', "z-index": "10" },
-          '100%': { width: '100%', "z-index": "10"},
+        'stape-line-active': {
+          '0%': { width: '0%', 'z-index': '10' },
+          '100%': { width: '100%', 'z-index': '10' },
         },
-        "stape-shape-active": {
+        'stape-shape-active': {
           '0%': { width: '0%' },
-          '100%': { width: '100%'},
+          '100%': { width: '100%' },
         },
-        "active-page": {
+        'active-page': {
           '0%': { opacity: '0', transform: 'translateY(50px)' },
-          '100%': { opacity: '1'},
+          '100%': { opacity: '1' },
+        },
+        "slide-active": {
+          '0%': {  height: '0px' },
+          '100%': { height: '77px' },
+        },
+        "slide-inactive": {
+          '0%': {  height: '77px' },
+          '100%': { height: '0px' },
         },
       },
       animation: {
         inactive: 'inactive .1s linear alternate forwards',
         active: 'active .1s linear alternate forwards',
-        "stape-line-active": 'stape-line-active 1.1s 0.6s linear alternate forwards',
-        "stape-shape-active": 'stape-shape-active 0.6s linear alternate forwards',
-        "active-page": 'active-page 0.6s linear alternate forwards',
+        "slide-active": 'slide-active 1.1s linear alternate forwards',
+        "slide-inactive": 'slide-inactive 1.1s linear alternate forwards',
+        'stape-line-active':'stape-line-active 1.1s 0.6s linear alternate forwards',
+        'stape-shape-active': 'stape-shape-active 0.6s linear alternate forwards',
+        'active-page': 'active-page 0.6s linear alternate forwards',
       },
       colors: {
         light: {
-          bg: {"400":"", "500":"", "600":"", "700":"" },
-          text: {"400":"", "500":"", "600":"", "700":"" },
-          primary: {"50":"#eff6ff", "100":"#dbeafe", "200":"#bfdbfe", "300":"#93c5fd", "400":"#60a5fa", "500":"#3b82f6", "600":"#2563eb", "700":"#1d4ed8", "800":"#1e40af", "900":"#1e3a8a"},
-          secondary: {"600":"#d8d8d8", "700":"#c0c0c0"}
+          bg: { 400: '', 500: '', 600: '', 700: '' },
+          text: { 400: '', 500: '', 600: '', 700: '' },
+          primary: {
+            50: '#eff6ff',
+            100: '#dbeafe',
+            200: '#bfdbfe',
+            300: '#93c5fd',
+            400: '#60a5fa',
+            500: '#3b82f6',
+            600: '#2563eb',
+            700: '#1d4ed8',
+            800: '#1e40af',
+            900: '#1e3a8a',
+          },
+          secondary: { 600: '#d8d8d8', 700: '#c0c0c0' },
         },
         dark: {
-          bg: {"400":"", "500":"", "600":"", "700":"#374151", "800":"#1f2937", "900":"#111827" },
-          text: {"100":"#F3F4F6", "200":"#E5E7EB", "300":"#D1D5DB", "400":"#9ca3af", "500":"", "600":"#2563eb", "700":"" },
-          primary: {"50":"#eff6ff", "100":"#dbeafe", "200":"#bfdbfe", "300":"#93c5fd", "400":"#60a5fa", "500":"#3b82f6", "600":"#2563eb", "700":"#1d4ed8", "800":"#1e40af", "900":"#1e3a8a"},
-          secondary: {"600":"#d8d8d8", "700":"#c0c0c0"}
+          bg: {
+            400: '',
+            500: '',
+            600: '',
+            700: '#374151',
+            800: '#1f2937',
+            900: '#111827',
+          },
+          text: {
+            100: '#F3F4F6',
+            200: '#E5E7EB',
+            300: '#D1D5DB',
+            400: '#9ca3af',
+            500: '',
+            600: '#2563eb',
+            700: '',
+          },
+          primary: {
+            50: '#eff6ff',
+            100: '#dbeafe',
+            200: '#bfdbfe',
+            300: '#93c5fd',
+            400: '#60a5fa',
+            500: '#3b82f6',
+            600: '#2563eb',
+            700: '#1d4ed8',
+            800: '#1e40af',
+            900: '#1e3a8a',
+          },
+          secondary: { 600: '#d8d8d8', 700: '#c0c0c0' },
         },
-        primary: {"50":"#eff6ff", "100":"#dbeafe", "200":"#bfdbfe", "300":"#93c5fd", "400":"#60a5fa", "500":"#3b82f6", "600":"#2563eb", "700":"#1d4ed8", "800":"#1e40af", "900":"#1e3a8a"},
-        secondary: {"600":"#d8d8d8", "700":"#c0c0c0"}
-      }
+        primary: {
+          50: '#eff6ff',
+          100: '#dbeafe',
+          200: '#bfdbfe',
+          300: '#93c5fd',
+          400: '#60a5fa',
+          500: '#3b82f6',
+          600: '#2563eb',
+          700: '#1d4ed8',
+          800: '#1e40af',
+          900: '#1e3a8a',
+        },
+        secondary: { 600: '#d8d8d8', 700: '#c0c0c0' },
+      },
     },
     fontFamily: {
-      'body': [
-      'Merriweather'
-    ],
-      'sans': [
-      'Merriweather'
-    ]
-  }},
-  plugins: [
-    require('./node_modules/flowbite/plugin')
-  ],
-}
+      body: ['Merriweather'],
+      sans: ['Merriweather'],
+    },
+  },
+  plugins: [require('./node_modules/flowbite/plugin')],
+};
