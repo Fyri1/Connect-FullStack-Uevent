52f10babd8f3b39a34c56c1d9ce2e0c18a9565a8
Herman <ushkalo.herman@gmail.com>
2023-04-12 02:52:40 +0300

    change axios on custom axios

--- client/src/components/auth/partner-register/partner-components/page-componenrs/FormPartner.jsx
+++ client/src/components/auth/partner-register/partner-components/page-componenrs/FormPartner.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+import $api from '../../../../../../utils/api.js'
 import { useNavigate } from 'react-router-dom';
 
 import InputField from '../../../../common/form/InputField.jsx';
