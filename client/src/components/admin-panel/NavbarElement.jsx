ef3f46581417c0fa214e11a69705374e0a6aaf47
Tymofeii <tima130603@gmail.com>
2023-04-11 17:30:12 -0400

    hyi

--- client/src/components/admin-panel/NavbarElement.jsx
+++ client/src/components/admin-panel/NavbarElement.jsx
@@ -3,11 +3,12 @@ import { useNavigate } from 'react-router-dom';
 
 import adminRoutes from "../../routes/client/adminRoutes.js";
 
+
 const NavbarElement = () => {
   const navigate = useNavigate();
 
   return (
-    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+    <nav className="pt-10 navbar navbar-expand-lg bg-dark navbar-dark">
       <div className="inline-flex rounded-md shadow-sm" role="group">
         <button onClick={() => { navigate(adminRoutes.usersPagePath()) }} type="button" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-l-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
           <svg aria-hidden="true" className="w-4 h-4 mr-2 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd"></path></svg>
