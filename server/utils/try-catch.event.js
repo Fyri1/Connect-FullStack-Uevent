d3c5ad92723f54bf74f214655362dfd60704db18
Herman <ushkalo.herman@gmail.com>
2023-04-12 02:49:52 +0300

    chages variable

--- server/utils/try-catch.event.js
+++ server/utils/try-catch.event.js
@@ -6,7 +6,7 @@ const tryCatch = (controller) => async (req, res, next) => {
       res.status(200).json({ values: result || 'Ok' });
   } catch (error) {
     if (error.code === 11000)
-      return res.json(ApiError.BadRequest('Validation error', errors.array()));
+      return res.json(ApiError.BadRequest('Validation error', error.array()));
     return next(error);
   }
 };
