faf288c95c292930bc3855f1dc4c0d7d4769d05f
Herman <ushkalo.herman@gmail.com>
2023-04-12 03:02:23 +0300

    add spinner button that request

--- client/src/components/auth/partner-register/partner-components/page-componenrs/FormPartner.jsx
+++ client/src/components/auth/partner-register/partner-components/page-componenrs/FormPartner.jsx
@@ -137,9 +137,21 @@ const FormPartner = ({ setCompleteStep, completeStep }) => {
                 </div>
               </TextField>
 
-              <div className="w-full text-center">
-                <button onClick={() => { console.log(partnerData) }} type="submit" className="w-[50%] px-5 mt-5 py-2.5 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                  Submit
+              <div className="w-full flex justify-center">
+                <button disabled={completeStep.step3.isComplete} type="submit" className="w-[50%] flex justify-center items-center px-5 mt-5 py-2.5 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                {!isLoadingButton ? (
+                'submit'
+                ) : (
+                  <>
+                    <svg className="-ml-1 mr-3 h-5 w-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                    <span className="font-medium subpixel-antialiased">
+                      Processing...
+                    </span>
+                  </>
+                )}
                 </button>
               </div>
             </div>
