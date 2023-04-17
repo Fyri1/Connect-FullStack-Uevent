3b17c230f8fb132e651e8cf48379a67a8e85a43e
Herman <ushkalo.herman@gmail.com>
2023-04-18 00:53:35 +0300

    :+1:

--- server/migrations/create_event_subscriptions_table.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-
-export const up = (knex) =>
-  knex.schema.createTable('event_subscriptions', (table) => {
-    table.string('user_id').unsigned().index().references('users.id');
-    table.string('event_id').unsigned().index().references('events.id');
-  });
-
-export const down = (knex) => knex.schema.dropTable('subscribe_events');
