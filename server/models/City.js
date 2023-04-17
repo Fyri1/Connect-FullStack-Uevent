96a237cb68b8e272c839cd394d40714b6463d195
Herman <ushkalo.herman@gmail.com>
2023-04-17 22:12:16 +0300

    Update City.js

--- server/models/City.js
+++ server/models/City.js
@@ -3,18 +3,19 @@ import path from 'path';
 
 class City {
   async getAll() {
-    const dataFile = await fsp.readFile(path.join(path.resolve(), 'ua-cities.json'), 'utf-8');
+    const dataFile = await fsp.readFile(
+      path.join(path.resolve(), 'ua-cities.json'),
+      'utf-8'
+    );
     const cities = JSON.parse(dataFile);
     return cities;
   }
-  async findCityName(title){
-    const allCity = await this.getAll()
-    const city = allCity.find(item => 
-    {   
-        return item.name.en === title + " region";
-        
-    } )
-    return city.name ;
+  async findCityName(title) {
+    const allCity = await this.getAll();
+    const city = allCity.find((item) => {
+      return item.name.en === title + ' region';
+    });
+    return city.name;
   }
 }
 
